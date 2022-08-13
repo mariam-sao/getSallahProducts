133e0cfd383aa8b307b040f7bbc4ac1a6a96a23e
Administrator <mabdullah0043@stu.kau.edu.sa>
2022-08-13 17:49:44 +0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+// Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
+
+Route::post('/auth/register', [AuthController::class, 'createUser']);
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+  try{
+
+    $curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://api.salla.dev/admin/v2/products",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => [
+    "Authorization: Bearer YyFkpLt8n2cr7KXZjCExDTbwB_3p2RYeweG6NSgE6MU.ISw6FTitwcdfeChxA9OejBS-q6EAcg4DoR9ABr7sc0w",
+    "Content-Type: application/json"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+  }catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+        
+
+});
